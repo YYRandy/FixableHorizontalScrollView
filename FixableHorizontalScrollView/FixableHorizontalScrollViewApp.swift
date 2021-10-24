c3bd67f73f695ccfab5e54d263f18c8ced4d06d2
Yajima Yohei <yajima.y.yohei@gmail.com>
2021-10-24 19:51:59 +0900

    Initial Commit

---
+++ FixableHorizontalScrollView/FixableHorizontalScrollViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  FixableHorizontalScrollViewApp.swift
+//  FixableHorizontalScrollView
+//
+//  Created by Yajima Youhei on 2021/10/24.
+//
+
+import SwiftUI
+
+@main
+struct FixableHorizontalScrollViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
